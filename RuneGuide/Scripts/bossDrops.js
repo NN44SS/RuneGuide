f171998f7aa67256aca0d1fbf42c0269deaad906
Nick Steinmetz <39065551+NN44SS@users.noreply.github.com>
2020-09-29 01:49:36 -0400

    Added all data for boss drops + template for DOM. Styling to come later.

--- RuneGuide/Scripts/bossDrops.js
+++ RuneGuide/Scripts/bossDrops.js
@@ -1,6 +1,7 @@
 let drops = [];
 
 
+
 function populateMonsterDrops(monsterId) {
   //Iterate over the monster list and pass values for the boss drop table
   for (let i = 0; i < monsterList.length; i++) {
@@ -21,9 +22,12 @@ function populateMonsterDrops(monsterId) {
     }
   }
   //sort the drop array by item rarity descending
-  drops.sort((a,b) => (a.dropRarity > b.dropRarity) ? -1: 1);
+  drops.sort((a, b) => (a.dropRarity) - (b.dropRarity));
+  createDropPanel();
   console.log(drops);
 }
+
+
 
 //format drop values and push to the drops array
 function populateDropsArray(dropID, dropName, dropQuantity, dropRarity){
@@ -41,6 +45,66 @@ let drop = {
   drops.push(drop);
 }
 
+
+
 function createDropPanel(){
-  //TODO - build template for the drops list to be populated.
+  var bossDropSection = document.getElementById('bossDropWraper');
+  var bossDropHeader = document.getElementById('bossDropHeader');
+  bossDropHeader.style.display = 'block';
+
+
+  for(let drop of drops){
+    //Create the row container to hold drop info divs
+    var bossDropRowWrapper = document.createElement('div');
+    bossDropRowWrapper.setAttribute('class', 'bossDropRow');
+
+    //Item img
+    var bossDropImgContainer = document.createElement('div');
+    bossDropImgContainer.setAttribute('class', 'bossDropImgContainer');
+    var dropImg = document.createElement('img');
+    dropImg.src = "https://www.osrsbox.com/osrsbox-db/items-icons/"+ drop.dropID +".png";
+    bossDropImgContainer.appendChild(dropImg);
+
+    //Item Name span
+    var bossDropNameContainer = document.createElement('div');
+    bossDropNameContainer.setAttribute('class', 'bossDropNameContainer');
+    var bossDropName = document.createElement('span');
+    bossDropName.innerHTML = drop.dropName;
+    bossDropNameContainer.appendChild(bossDropName);
+
+    //Item quantity span
+    var bossDropQuantityContainer = document.createElement('div');
+    bossDropQuantityContainer.setAttribute('class', 'bossDropQuantityContainer');
+    var bossDropQuantity = document.createElement('span');
+    bossDropQuantity.innerHTML = drop.dropQuantity;
+    bossDropQuantityContainer.appendChild(bossDropQuantity);
+
+    //Item rarity span
+    var bossDropRarityContainer = document.createElement('div');
+    bossDropRarityContainer.setAttribute('class', 'bossDropRarityContainer');
+    var bossDropRarity = document.createElement('span');
+    bossDropRarity.innerHTML = drop.dropRarity;
+    bossDropRarityContainer.appendChild(bossDropRarity);
+
+    //Append properties to row wrapper then append that to the DOM 'bossDropWraper'
+    bossDropRowWrapper.appendChild(bossDropImgContainer);
+    bossDropRowWrapper.appendChild(bossDropNameContainer);
+    bossDropRowWrapper.appendChild(bossDropQuantityContainer);
+    bossDropRowWrapper.appendChild(bossDropRarityContainer);
+    bossDropSection.appendChild(bossDropRowWrapper);
+
+
+    // console.log(drop.dropID);
+  }
 }
+
+
+// var requestDropIcon = async (id) => {
+
+//   let url = 'https://api.osrsbox.com/icons_items?where={"id":'+ id +'}';
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   return data;
+//   requestDropIcon(drop.dropID).then(data => console.log(data));
+//   // console.log(data);
+// }
---
+++ RuneGuide/Scripts/contentControls.js
@@ -0,0 +1,9 @@
+function toggleDropSection(){
+  var bossDropSection = document.getElementById('bossDropWraper');
+
+  if (bossDropSection.style.display === 'none') {
+    bossDropSection.style.display = 'block';
+  }else {
+    bossDropSection.style.display = 'none';
+  }
+}
