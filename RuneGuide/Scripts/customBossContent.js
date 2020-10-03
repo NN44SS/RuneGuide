b4d853d6a7871570161fab8f6ace8c5b2815bba2
Nick Steinmetz <39065551+NN44SS@users.noreply.github.com>
2020-10-02 23:59:18 -0400

    Started adding additions to customBossContent. WIP

--- RuneGuide/Scripts/customBossContent.js
+++ RuneGuide/Scripts/customBossContent.js
@@ -1,126 +1,185 @@
-const recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+
+let recommendedStatsList = [];
 
 // var dropImg = document.createElement('img');
 // dropImg.setAttribute('class', 'bossDropDataTxt');
 // dropImg.src = "https://www.osrsbox.com/osrsbox-db/items-icons/"+ drop.dropID +".png";
 
 
-function kbdData{
+function kbdData(){
+  var recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+  recommendedStatsList.push('Images/SkillBubbles/Att80.png');
+  recommendedStatsList.push('Images/SkillBubbles/Strength80.png');
+  recommendedStatsList.push('Images/SkillBubbles/Def80.png');
+  recommendedStatsList.push('Images/SkillBubbles/Range80.png');
+
+  for(let req of recommendedStatsList){
+    var reqImg = document.createElement('img');
+    reqImg.src = req;
+    recommendedStatsSection.appendChild(reqImg);
+  }
+}
+
+function corpData(){
+  var recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+  recommendedStatsList.push('Images/SkillBubbles/Att90.png');
+  recommendedStatsList.push('Images/SkillBubbles/Strength90.png');
+  recommendedStatsList.push('Images/SkillBubbles/Def90.png');
+  recommendedStatsList.push('Images/SkillBubbles/Range90.png');
+
+  for(let req of recommendedStatsList){
+    var reqImg = document.createElement('img');
+    reqImg.src = req;
+    recommendedStatsSection.appendChild(reqImg);
+  }
+}
+
+function krakenData(){
+  var recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+  recommendedStatsList.push('Images/SkillBubbles/Def70.png');
+  recommendedStatsList.push('Images/SkillBubbles/Mage75.png');
+
+  for(let req of recommendedStatsList){
+    var reqImg = document.createElement('img');
+    reqImg.src = req;
+    recommendedStatsSection.appendChild(reqImg);
+  }
+}
+
+function thermyData(){
+  var recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+  recommendedStatsList.push('Images/SkillBubbles/Att90.png');
+  recommendedStatsList.push('Images/SkillBubbles/Strength90.png');
+  recommendedStatsList.push('Images/SkillBubbles/Def90.png');
+  recommendedStatsList.push('Images/SkillBubbles/Mage75.png');
+
+  for(let req of recommendedStatsList){
+    var reqImg = document.createElement('img');
+    reqImg.src = req;
+    recommendedStatsSection.appendChild(reqImg);
+  }
+}
+
+function kqData(){
+  var recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+  recommendedStatsList.push('Images/SkillBubbles/Att85.png');
+  recommendedStatsList.push('Images/SkillBubbles/Strength85.png');
+  recommendedStatsList.push('Images/SkillBubbles/Def70.png');
+  recommendedStatsList.push('Images/SkillBubbles/Range85.png');
+
+  for(let req of recommendedStatsList){
+    var reqImg = document.createElement('img');
+    reqImg.src = req;
+    recommendedStatsSection.appendChild(reqImg);
+  }
+}
+
+function zulrahData(){
+  var recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+  recommendedStatsList.push('Images/SkillBubbles/Mage80.png');
+  recommendedStatsList.push('Images/SkillBubbles/Range80.png');
+  recommendedStatsList.push('Images/SkillBubbles/Def70.png');
+  recommendedStatsList.push('Images/SkillBubbles/Range90.png');
+
+  for(let req of recommendedStatsList){
+    var reqImg = document.createElement('img');
+    reqImg.src = req;
+    recommendedStatsSection.appendChild(reqImg);
+  }
+}
+
+function chaosEleData(){
 
 }
 
-function corpData{
+function commZilData(){
 
 }
 
-function krakenData{
+function genGraarData(){
 
 }
 
-function thermyData{
+function dagSupData(){
 
 }
 
-function kqData{
+function dagPrimeData(){
 
 }
 
-function zulrahData{
+function dagRexData(){
 
 }
 
-function chaosEleData{
+function jadData(){
 
 }
 
-function commZilData{
+function zammyData(){
 
 }
 
-function genGraarData{
+function kreeData(){
 
 }
 
-function dagSupData{
+function moleData(){
 
 }
 
-function dagPrimeData{
+function cerbData(){
 
 }
 
-function dagRexData{
+function sireData(){
 
 }
 
-function jadData{
+function callistoData(){
 
 }
 
-function zammyData{
+function venenatisData(){
 
 }
 
-function kreeData{
+function vetionData(){
 
 }
 
-function moleData{
+function scorpiaData(){
 
 }
 
-function cerbData{
+function crazyArchData(){
 
 }
 
-function sireData{
+function chaosFanData(){
 
 }
 
-function callistoData{
+function skotizoData(){
 
 }
 
-function venenatisData{
+function duskData(){
 
 }
 
-function vetionData{
+function vorkathData(){
 
 }
 
-function scorpiaData{
+function hesporiData(){
 
 }
 
-function crazyArchData{
+function hydraData(){
 
 }
 
-function chaosFanData{
-
-}
-
-function skotizoData{
-
-}
-
-function duskData{
-
-}
-
-function vorkathData{
-
-}
-
-function hesporiData{
-
-}
-
-function hydraData{
-
-}
-
-function sarachData{
+function sarachData(){
 
 }
