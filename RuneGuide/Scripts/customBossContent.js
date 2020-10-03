96dfa40d4bca80c599f2607d069dbf2d9a7f94bb
Nick Steinmetz <39065551+NN44SS@users.noreply.github.com>
2020-10-02 20:59:51 -0400

    Created a new file to hold custom data for each boss. WIP

---
+++ RuneGuide/Scripts/customBossContent.js
@@ -0,0 +1,126 @@
+const recommendedStatsSection = document.getElementById('RecommendedStatsContainer');
+
+// var dropImg = document.createElement('img');
+// dropImg.setAttribute('class', 'bossDropDataTxt');
+// dropImg.src = "https://www.osrsbox.com/osrsbox-db/items-icons/"+ drop.dropID +".png";
+
+
+function kbdData{
+
+}
+
+function corpData{
+
+}
+
+function krakenData{
+
+}
+
+function thermyData{
+
+}
+
+function kqData{
+
+}
+
+function zulrahData{
+
+}
+
+function chaosEleData{
+
+}
+
+function commZilData{
+
+}
+
+function genGraarData{
+
+}
+
+function dagSupData{
+
+}
+
+function dagPrimeData{
+
+}
+
+function dagRexData{
+
+}
+
+function jadData{
+
+}
+
+function zammyData{
+
+}
+
+function kreeData{
+
+}
+
+function moleData{
+
+}
+
+function cerbData{
+
+}
+
+function sireData{
+
+}
+
+function callistoData{
+
+}
+
+function venenatisData{
+
+}
+
+function vetionData{
+
+}
+
+function scorpiaData{
+
+}
+
+function crazyArchData{
+
+}
+
+function chaosFanData{
+
+}
+
+function skotizoData{
+
+}
+
+function duskData{
+
+}
+
+function vorkathData{
+
+}
+
+function hesporiData{
+
+}
+
+function hydraData{
+
+}
+
+function sarachData{
+
+}
